a807e409eff8b6662383ecdd4aa3fe573b6dd72f
Charles Bryant <cwbryant25@yahoo.com>
2024-01-25 13:38:04 -0800

    Updated JS

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /**
  * @param {string[]} string - answers based on favorite flavors
- * @returns {string} the number of flavors they have ordered
+ * @returns {object} the number of flavors they have ordered
  */
 
 /*
 User enters flavors from a prompt
 Split the answers into an array
+Declare a function with the parameter of the flavor array
 Initalize a variable to store their answers
 Iterate through the array
 Store each iteration in the variable
@@ -15,8 +16,22 @@ Return flavor list to user
 
 // Prompt the user for a list of integers separated by commas.
 const userFlavorString = prompt(
-  "Please enter how many of your favorite yogurt flavors you would like to order, separated by commas...i.e.",
-  "chocolate,vanilla,vanilla,strawberry"
+  "Please enter how many of your favorite yogurt flavors you would like to order, separated by commas...i.e. chocolate,vanilla,vanilla,strawberry"
 );
 
 const yogurtArray = userFlavorString.split(",");
+
+console.log(yogurtArray);
+
+function userFlavorList(array) {
+  let order = {};
+  for (const element of array) {
+    if (!element in order) {
+      order.splice(0);
+    } else {
+    }
+  }
+  return order;
+}
+
+console.log(userFlavorList(yogurtArray));
